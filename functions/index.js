e187cf1a6133c722a8d23d88b4bd5168b2ec95b8
Eric Bello <ebello219@gmail.com>
2023-09-19 13:02:38 -0400

    updated functions

--- functions/index.js
+++ functions/index.js
@@ -27,3 +27,7 @@ exports.deleteOldItems = functions.database.ref('/media/')
     return ref.update(updates);
   });
 });
+
+exports["myfunc"] = runWith({secrets: ["REACT_APP_TMDB_API_KEY"]}).https.onCall(() => {
+  process.env.REACT_APP_TMDB_API_KEY
+})  
