0c9a719220e0f96a5e60086fc9998281a0cc3cb1
Eric Bello <ebello219@gmail.com>
2023-09-21 14:22:21 -0400

    added scheduler function

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ initializeApp();
 
 exports.deleteOldItems = functions.database.ref('/media/{id}')
 .onWrite((change, context) => {
-    const cutoff = Date.now() - (1 * 60 * 60 * 1000); // 1 hour in milliseconds
+    const cutoff = Date.now() - (2 * 60 * 60 * 1000); // 2 hours in milliseconds
 
     // If a new item is created or an existing item is updated
     if (change.after.exists() && (!change.before.exists() || change.after.val().timestamp !== change.before.val().timestamp)) {
@@ -27,5 +27,27 @@ exports.deleteOldItems = functions.database.ref('/media/{id}')
         });
 
         return ref.parent.update(updates);
+    });
+});
+
+// Schedule function to delete old items even if no new entries are added
+exports.scheduledFunction = functions.pubsub.schedule('every 24 hours').timeZone('America/New_York').onRun((context) => {
+    const cutoff = Date.now() - (2 * 60 * 60 * 1000); // 2 hours in milliseconds
+
+    const ref = admin.database().ref('/media');
+
+    // Query for old items
+    const oldItemsQuery = ref.orderByChild('timestamp').endAt(cutoff);
+
+    return oldItemsQuery.once('value').then(snapshot => {
+        const updates = {};
+
+        snapshot.forEach(child => {
+            if (child.val().timestamp <= cutoff) {
+                updates[child.key] = null;
+            }
+        });
+
+        return ref.update(updates);
     });
 });
