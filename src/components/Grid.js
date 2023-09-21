b4a76257efc7bc3e79784615328a5d94ed5d7f6c
Eric Bello <ebello219@gmail.com>
2023-09-21 18:28:54 -0400

    updatted timestamp variable

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -136,7 +136,7 @@ export default function Grid(props) {
       overview: description,
       media_type: category,
       poster_path: imageUrl,
-      "timestamp": "timestamp"    
+      timestamp: {".sv": "timestamp"} // Set a server timestamp
     });
   }
 
